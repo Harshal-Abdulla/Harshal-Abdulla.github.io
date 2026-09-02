665bd1367fa0756a46fb095dd461a0aaadeb018f
Harshal Abdulla <harshalabdulla1999@gmail.com>
2026-09-02 19:28:32 +0100

    CV is 103 KB now, not 110

--- content/profile.ts
+++ content/profile.ts
@@ -62,7 +62,7 @@ export const PROFILE = {
    *   page all read these two values.
    */
   cvPath: "/Harshal-Abdulla-CV-2026-09.pdf",
-  cvSize: "110 KB",
+  cvSize: "103 KB",
 
   education: [
     {
